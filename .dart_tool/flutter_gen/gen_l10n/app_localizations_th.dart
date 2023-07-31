71e71e17db1f584fc0e1850f3a55f6d63f520897
nkhanh44 <nkhanh4496@gmail.com>
2023-07-31 11:54:52 +0700

    fixed untracked files

--- .dart_tool/flutter_gen/gen_l10n/app_localizations_th.dart
+++
@@ -1,9 +0,0 @@
-import 'app_localizations.dart';
-
-/// The translations for Thai (`th`).
-class AppLocalizationsTh extends AppLocalizations {
-  AppLocalizationsTh([String locale = 'th']) : super(locale);
-
-  @override
-  String get hello => 'สวัสดี';
-}
