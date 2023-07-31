2a3b528d2374efb8a985dd3a6bb9dee3b5f98912
nkhanh44 <nkhanh4496@gmail.com>
2023-07-31 11:30:42 +0700

    [#1, #3] Init project

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_th.dart
@@ -0,0 +1,9 @@
+import 'app_localizations.dart';
+
+/// The translations for Thai (`th`).
+class AppLocalizationsTh extends AppLocalizations {
+  AppLocalizationsTh([String locale = 'th']) : super(locale);
+
+  @override
+  String get hello => 'สวัสดี';
+}
